c3c42d4c9368b65bd030e785ba16729a91156cb4
cuebin <beknosee@outlook.com>
2023-05-29 17:27:23 +0200

    .

---
+++ lua/custom/plugins/context.lua
@@ -0,0 +1,7 @@
+return {
+
+  vim.keymap.set("n", "[c", function()
+  require("treesitter-context").go_to_context()
+  end, { silent = true })
+}
+
---
+++ lua/custom/plugins/transparent.lua
@@ -0,0 +1,7 @@
+return {                                                                                                                                    
+  "xiyaowong/nvim-transparent",
+    config = function () 
+    require("transparent").setup {
+    }                           
+  end, 
+}   
